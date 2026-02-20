4ce3223ae653a4572a1f8a693763f82f95ace465
simukim <sorenkim2011@gmail.com>
2026-02-20 11:01:12 -0500

    tests and fixes

--- labs/lab_1/lab_1d.py
+++ labs/lab_1/lab_1d.py
@@ -23,7 +23,7 @@ def two_sum(nums: list[int], target: int) -> list[int]:
 
     num_to_index = {}
     for index, num in enumerate(nums):
-        complement = target + num
+        complement = target - num
         if complement in num_to_index:
             return [num_to_index[complement], index]
         num_to_index[num] = index
@@ -32,7 +32,7 @@ def two_sum(nums: list[int], target: int) -> list[int]:
 # Example usage:
 def main():
     nums = [2, 7, 11, 15]
-    target = 9
+    target = 26
     result = two_sum(nums, target)
     print(f"Indices of the two numbers that add up to {target}: {result}")
 
